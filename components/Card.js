ad2112cfc973cbbabfdea3ccac9a6c8f16cd556f
Lun Lun <luanhua8888@gmail.com>
2022-11-05 22:48:04 +0700

    Vercell2

--- components/Card.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Card() {
-  return (
-    <div className='flex flex-col justify-center items-center bg-sky-100 mr-70 ml-70 mt-40 mb-20 m-60 pb-5 pt-10 rounded-lg border-black border-2'></div>
-  )
-}
-
-export default Card
